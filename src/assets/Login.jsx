44be81fb35736fee704db0e76d403c94c37e5658
Gopal Krishan <70444883+gkrishna16@users.noreply.github.com>
2022-07-12 05:33:06 -0400

    added context

--- src/assets/Login.jsx
+++ src/assets/Login.jsx
@@ -1,34 +1,39 @@
 import React, { useState, useContext } from "react";
-
+import { LoginContext } from "../context/LoginContext";
 
 const Login = () => {
+  const { userName, setUserName, setShowProfile, showProfile } =
+    useContext(LoginContext);
+
+  console.log(userName);
+
   return (
     <div>
-      <form>
-        {/* <label>Username</label> */}
-        <input
-          type="text"
-          placeholder="Username"
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }}
-        />{" "}
-        <input
-          type="text"
-          placeholder="Password..."
-          onChange={(event) => {
-            setUsername(event.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            setShowProfile(true);
-          }}
-        >
-          LOGIN
-        </button>
-      </form>
-      {setShowProfile && <h1>{userName}</h1>}
+      {/* <form> */}
+      {/* <label>Username</label> */}
+      <input
+        type="text"
+        placeholder="Username"
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />{" "}
+      <input
+        type="text"
+        placeholder="Password..."
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setShowProfile(true);
+        }}
+      >
+        LOGIN
+      </button>
+      {/* </form> */}
+      {/* {setShowProfile && <h1>{userName}</h1>} */}
     </div>
   );
 };
--- src/assets/Profile.jsx
+++ src/assets/Profile.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import { LoginContext } from "../context/LoginContext";
 
 const Profile = () => {
+  const { userName } = useContext(LoginContext);
+
   return (
     <div>
       <h1>Profile</h1>
-      <h2>Username :</h2>
+      <h2>{userName}</h2>
     </div>
   );
 };
